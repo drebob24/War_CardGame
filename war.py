aa0d2af1ad2e169a15c0f2eb713252465ba70821
Andrius Bobbit <abobbit@gmail.com>
2024-12-16 09:56:20 +0000

    Initial commit for war.py

---
+++ war.py
@@ -0,0 +1,17 @@
+class Card():
+    ...
+
+class Deck():
+    ...
+
+def main():
+    #Create list of 52 cards
+    cards = create_cards()
+    #Splits cards into user and cpu decks.
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
