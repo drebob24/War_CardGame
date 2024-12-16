fe7a9c07b45bdaf34bb3cc9eec1c47c672a32a43
Andrius Bobbit <abobbit@gmail.com>
2024-12-16 22:51:54 +0000

    Made create_cards() more modular

--- war.py
+++ war.py
@@ -11,19 +11,25 @@ class Deck():
 
 def main():
     #Create list of 52 cards
-    cards = create_cards()
+    cards = create_cards(get_suits(), get_faces())
     #Splits cards into user and cpu decks.
     
 
-def create_cards():
-    suits = ["♢", "♡", "♠️", "♣️"]
-    faces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+def create_cards(suits, faces):
     card_list = []
     for suit in suits:
         for face in faces:
             card = Card(face, suit, faces.index(face))
             card_list.append(card)
     return card_list
+
+
+def get_suits():
+    return ["♢", "♡", "♠️", "♣️"]
+
+
+def get_faces():
+    return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 
 if __name__ == "__main__":
